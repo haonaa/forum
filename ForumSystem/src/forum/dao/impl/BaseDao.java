6b38f9de4eeed4eba108b5dd424f8b7ecba000bd
haonaa <35794935+haonaa@users.noreply.github.com>
2018-01-26 15:36:42 +0800

    练习
    未加入hibernate

---
+++ ForumSystem/src/forum/dao/impl/BaseDao.java
@@ -0,0 +1,11 @@
+package forum.dao.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.orm.hibernate3.HibernateTemplate;
+
+public class BaseDao {
+	@Resource
+	protected HibernateTemplate hibernateTemplate;
+
+}
